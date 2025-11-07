766c836a3a1020dd5f218200a9f329787fd282fc
Julen <j-solla@etxepare.eus>
2025-11-07 17:20:40 +0100

    adding timestamp toggle

--- chat.py
+++ chat.py
@@ -1,8 +1,11 @@
 from datetime import datetime
+from discord import Embed
 from discord.ext import tasks, commands
 from file_read_backwards import FileReadBackwards
 import glob
 import re
+
+import embed
 
 
 class ChatHandler(commands.Cog):
@@ -16,14 +19,14 @@ class ChatHandler(commands.Cog):
         self.update.start()
         self.webhook = None
 
-    def splitLine(self, line: str):
+    def splitLine(self, line: str) -> tuple[datetime, str]:
         """Split a log line into a timestamp and the remaining message"""
         timestampStr, message = line.strip()[1:].split("]", 1)
         timestamp = datetime.strptime(timestampStr, "%d-%m-%y %H:%M:%S.%f")
         return timestamp, message
 
     @tasks.loop(seconds=2)
-    async def update(self):
+    async def update(self) -> None:
         """Update the handler
 
         This will check the latest log file and update our data based on any
@@ -43,7 +46,7 @@ class ChatHandler(commands.Cog):
                         break
                 self.lastUpdateTimestamp = newTimestamp
 
-    async def handleLog(self, timestamp: datetime, message: str):
+    async def handleLog(self, timestamp: datetime, message: str) -> Embed | None:
         """Parse the given line from the logfile and mirror chat message in
         discord if necessary"""
 
@@ -55,19 +58,24 @@ class ChatHandler(commands.Cog):
             # Exit if server message handling setting is disabled
             if match_data.group(1) == "Server" and not self.serverMessages:
                 return
-            # Use a webhook to make it look like we're the discord member
-            # God bless stack overflow
-            if self.bot.channel:
-                for webhook in await self.bot.channel.webhooks():
-                    if webhook.user == self.bot.user:
-                        self.webhook = webhook
-            if self.webhook is None:
-                self.webhook = await self.bot.channel.create_webhook(name="zomboi")
-            name = match_data.group(1)
-            avatar_url = None
-            for member in self.bot.get_all_members():
-                if match_data.group(1) in member.name:
-                    avatar_url = member.display_avatar
-            await self.webhook.send(
-                match_data.group(2), username=name, avatar_url=avatar_url
-            )
+
+            if match and self.bot.channel is not None:
+                # Use a webhook to make it look like we're the discord member
+                # God bless stack overflow
+                if self.bot.channel:
+                    for webhook in await self.bot.channel.webhooks():
+                        if webhook.user == self.bot.user:
+                            self.webhook = webhook
+                if self.webhook is None:
+                    self.webhook = await self.bot.channel.create_webhook(name="zomboi")
+
+                name = match_data.group(1)
+                avatar_url = None
+                for member in self.bot.get_all_members():
+                    if match_data.group(1) in member.name:
+                        avatar_url = member.display_avatar
+                await self.webhook.send(
+                    embed=embed.chat_message(timestamp, match_data.group(2)),
+                    username=name,
+                    avatar_url=avatar_url,
+                )
