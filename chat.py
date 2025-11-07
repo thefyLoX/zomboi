a41bebb2dda48d9a8406507e4e739f982ca09c3b
Julen <j-solla@etxepare.eus>
2025-11-07 14:38:49 +0100

    hide admin logon/off notifications

--- chat.py
+++ chat.py
@@ -1,11 +1,8 @@
 from datetime import datetime
-from discord import Embed
 from discord.ext import tasks, commands
 from file_read_backwards import FileReadBackwards
 import glob
 import re
-
-import embed
 
 
 class ChatHandler(commands.Cog):
@@ -18,14 +15,14 @@ class ChatHandler(commands.Cog):
         self.update.start()
         self.webhook = None
 
-    def splitLine(self, line: str) -> tuple[datetime, str]:
+    def splitLine(self, line: str):
         """Split a log line into a timestamp and the remaining message"""
         timestampStr, message = line.strip()[1:].split("]", 1)
         timestamp = datetime.strptime(timestampStr, "%d-%m-%y %H:%M:%S.%f")
         return timestamp, message
 
     @tasks.loop(seconds=2)
-    async def update(self) -> None:
+    async def update(self):
         """Update the handler
 
         This will check the latest log file and update our data based on any
@@ -33,7 +30,7 @@ class ChatHandler(commands.Cog):
         """
         files = glob.glob(self.logPath + "/*chat.txt")
         if len(files) > 0:
-            with FileReadBackwards(files[0], encoding="utf-8") as f:
+            with FileReadBackwards(files[0], encoding="latin-1") as f:
                 newTimestamp = self.lastUpdateTimestamp
                 for line in f:
                     timestamp, message = self.splitLine(line)
@@ -45,47 +42,51 @@ class ChatHandler(commands.Cog):
                         break
                 self.lastUpdateTimestamp = newTimestamp
 
-    async def handleLog(self, timestamp: datetime, message: str) -> Embed | None:
+    async def handleLog(self, timestamp: datetime, message: str):
         """Parse the given line from the logfile and mirror chat message in
         discord if necessary"""
 
-        # Example message received
-        # [info] Message ChatMessage{chat=General, author='fyLoX', text='testingtesting'} sent to chat (id = 0) members.
-        # Ignore anything that's not "General" chat
-        if "chat=General" not in message:
-        # PATCH 1line #
-        # if "chat=General" or "Server alert message: '" not in message:
-            return
+        # # Ignore anything that's not "General" chat
+        # if "chat=General" not in message:
+        #     return
 
-        # Mirror any other received messages in the discord chat
-        pattern = r"] Message.*author=\'(.*)\', text=\'(.*)\'"
-        match = re.search(pattern, message)
-
-        # PATCH 2 lines#
-        # match_patch = re.search(r"^(?:\[info\] Message ChatMessage\{chat=General, author=|\] Server alert message: )\'", message)
-        # if match_patch and self.bot.channel is not None:
-
-        if match and self.bot.channel is not None:
+        # # Mirror any other received messages in the discord chat
+        # pattern = r"] Message.*author=\'(.*)\', text=\'(.*)\'"
+        # match = re.search(pattern, message)
+        # if match and self.bot.channel is not None:
+        #     # Use a webhook to make it look like we're the discord member
+        #     # God bless stack overflow
+        #     if self.bot.channel:
+        #         for webhook in await self.bot.channel.webhooks():
+        #             if webhook.user == self.bot.user:
+        #                 self.webhook = webhook
+        #     if self.webhook is None:
+        #         self.webhook = await self.bot.channel.create_webhook(name="zomboi")
+        #     name = match.group(1)
+        #     avatar_url = None
+        #     for member in self.bot.get_all_members():
+        #         if match.group(1) in member.name:
+        #             avatar_url = member.avatar_url
+        #     await self.webhook.send(
+        #         match.group(2), username=name, avatar_url=avatar_url
+        #     )
+        match = re.search(r"^(?:\[info\] Message ChatMessage\{chat=General, author=| Server alert message: )\'", message)
+        if  match and self.bot.channel is not None:
+            pattern = r"(?:] Message.*chat=General, author=\'|^ )(.*)(?:\', text=| alert message: )\'(.*)\'"
+            match_data = re.search(pattern, message)
             # Use a webhook to make it look like we're the discord member
             # God bless stack overflow
-            # PATCH 2 lines #
-            # pattern = r"(?:] Message.*chat=General, author=\'|^ )(.*)(?:\', text=| alert message: )\'(.*)\'"
-            # match = re.search(pattern, message)
             if self.bot.channel:
                 for webhook in await self.bot.channel.webhooks():
                     if webhook.user == self.bot.user:
                         self.webhook = webhook
             if self.webhook is None:
                 self.webhook = await self.bot.channel.create_webhook(name="zomboi")
-
-            name = match.group(1)
+            name = match_data.group(1)
             avatar_url = None
             for member in self.bot.get_all_members():
-                if match.group(1) in member.name:
+                if match_data.group(1) in member.name:
                     avatar_url = member.display_avatar
-
             await self.webhook.send(
-                embed=embed.chat_message(timestamp, message),
-                username=name,
-                avatar_url=avatar_url,
+                match_data.group(2), username=name, avatar_url=avatar_url
             )
